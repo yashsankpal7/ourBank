e0de36c737737b300c902913955e769f7952cbab
Yashsankpal <53860409+Yashsankpal@users.noreply.github.com>
2022-07-14 23:50:28 +0530

    title + service: done

---
+++ index.js
@@ -0,0 +1,52 @@
+function time() {
+  let time = new Date();
+  let hour = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+  am_pm = "AM";
+  
+  if (hour > 12) {
+      hour -= 12;
+      am_pm = " PM";
+  }
+  if (hour == 0) {
+      hr = 12;
+      am_pm = " AM";
+  }
+  
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+  
+  let currentTime = hour + ":" 
+  + min + ":" + sec + am_pm;
+  
+  document.title = currentTime;
+}
+window.setInterval(time, 1000);
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
