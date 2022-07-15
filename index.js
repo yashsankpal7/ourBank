d98863675fc579c8b213f36578eed847ded9b40b
Yashsankpal <53860409+Yashsankpal@users.noreply.github.com>
2022-07-15 11:34:20 +0530

    done

--- index.js
+++ index.js
@@ -25,28 +25,37 @@ function time() {
 }
 window.setInterval(time, 1000);
 
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function change(params) {
+  let fixedRate = {
+    'home':7,
+    'car':9,
+    'personal':12
   }
+
+  let fixedYears = {
+    'home':25,
+    'car':7,
+    'personal': 5
+  }
+
+  document.forms['Loan']['roi'].value = fixedRate[document.forms['Loan']['typeSelect'].value]   
+
+  document.forms['Loan']['duration'].min = 1
+  document.forms['Loan']['duration'].max = fixedYears[document.forms['Loan']['typeSelect'].value]
+}
+
+function calc(params) {
+  
+  let p = parseInt(document.forms['Loan']['amt'].value)
+
+  let r = parseInt(document.forms['Loan']['roi'].value)
+
+  let n = parseInt(document.forms['Loan']['duration'].value)
+
+  document.forms['Loan']['emi'].value =  p*r*(Math.pow(1+r, n))/(Math.pow(1+r,n)-1);
+
 }
